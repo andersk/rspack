ceaae8252081820bdc74a59d4f140ca2617a5152
hardfist <yangjianzju@gmail.com>
2024-10-29 10:49:28 +0800

    chore: use async thread pool

--- crates/rspack_loader_runner/src/runner.rs
+++ crates/rspack_loader_runner/src/runner.rs
@@ -4,7 +4,6 @@ use rspack_error::{error, IntoTWithDiagnosticArray, Result, TWithDiagnosticArray
 use rspack_fs::ReadableFileSystem;
 use rspack_sources::SourceMap;
 use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
-use tokio::task::spawn_blocking;
 
 use crate::{
   content::{AdditionalData, Content, ResourceData},
@@ -44,10 +43,9 @@ async fn process_resource<Context: Send>(
     {
       let resource_path_owned = resource_path.to_owned();
       // use spawn_blocking to avoid block,see https://docs.rs/tokio/latest/src/tokio/fs/read.rs.html#48
-      let result = spawn_blocking(move || fs.read(resource_path_owned.as_std_path()))
+      let result = tokio::fs::read(resource_path_owned)
         .await
-        .map_err(|e| error!("{e}, spawn task failed"))?;
-      let result = result.map_err(|e| error!("{e}, failed to read {resource_path}"))?;
+        .map_err(|e| error!("{e}, failed to read {resource_path}"))?;
       loader_context.content = Some(Content::from(result));
     } else if !resource_data.get_scheme().is_none() {
       let resource = &resource_data.resource;
